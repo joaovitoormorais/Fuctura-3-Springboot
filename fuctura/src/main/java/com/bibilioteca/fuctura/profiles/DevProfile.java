e271aabe3f70914c9137d7233eec68aa1ea86fcf
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2024-08-20 12:58:45 -0300

    criação do perfil de dev

---
+++ fuctura/src/main/java/com/bibilioteca/fuctura/profiles/DevProfile.java
@@ -0,0 +1,22 @@
+package com.bibilioteca.fuctura.profiles;
+
+import com.bibilioteca.fuctura.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevProfile {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instanciaDB() {
+        this.dbService.instanciaDB();
+    }
+
+
+}
